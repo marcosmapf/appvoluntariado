419d7d2d04c2ab2e10d9126df254bc18f44da7fb
Marcos de Andrade <marcosandradeapf@gmail.com>
2018-10-29 05:44:57 -0300

    Adding PUT/Update functionality to Volunteer Resource

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,30 +1,50 @@
 from rest_framework import generics, status
+from django.shortcuts import get_object_or_404
 from .models import Volunteer
 from .serializers import VolunteerSerializer
 
 from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 class VolunteerMixin:	
 
 	serializer_class = VolunteerSerializer
 	permission_classes = ()
+	renderer_classes = (JSONRenderer, )
 
 	def get_queryset(self):
 		return Volunteer.objects.all()
 
 
 class VolunteerRetrieveUpdate(VolunteerMixin, generics.RetrieveUpdateAPIView):
-	pass
-	#def put(self, request, pk, format=None):
-		#queryset = Volunteer.objects.all()
-		#get_object_or_404(queryset, pk=pk)
-		#serializer = SnippetSerializer(snippet, data=request.data)
-		#if serializer.is_valid():
-			#serializer.save()
-			#return Response(serializer.data)
-		#return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	
+	def put(self, request, pk, format=None):
+		queryset = self.get_queryset()
+		volunteer = get_object_or_404(queryset, pk=pk)
+		volunteer_data = VolunteerSerializer(volunteer).data
 
+		if 'interest_areas' in request.data:
+			interest_areas = request.data.pop("interest_areas")
+		else:
+			interest_areas = []
+			for key in volunteer_data['interest_areas']:
+				interest_areas.append(key.get('id'))
+
+		for field in request.data:
+			volunteer_data[field] = request.data.get(field)			
+
+		modified_serializer = VolunteerSerializer(data=volunteer_data)
+
+		if modified_serializer.is_valid():
+			self.perform_update(modified_serializer, interest_areas)
+			return Response(modified_serializer.data, status=status.HTTP_200_OK)
+
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def perform_update(self, serializer, interest_areas):
+		volunteer = serializer.save()
+		volunteer.interest_areas.add(*interest_areas)
 
 class VolunteerListCreate(VolunteerMixin, generics.ListCreateAPIView):
 
